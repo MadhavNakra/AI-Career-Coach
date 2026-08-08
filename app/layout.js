36e51b0f627795e8c32b50c709985b333a3320c9
MadhavNakra <madhav.nakra2003@gmail.com>
2026-08-08 18:03:41 +0530

    Onboarding page done

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({ children }) {
           >
             <Header />
             <main className="min-h-screen">{children}</main>
+            <Toaster richColors />
             <footer className="bg-muted/50 py-12">
               <p className="container mx-auto px-4 text-center text-gray-200">
                 @Copyright 2026 AI Career Coach. All rights reserved.
