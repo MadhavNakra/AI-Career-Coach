feef467d27ccb4dc7cb74dc9d2d94ebc468f10de
MadhavNakra <madhav.nakra2003@gmail.com>
2026-08-10 22:26:51 +0530

    Industry Insights working fine

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,9 @@
 import { serve } from "inngest/next";
+
 import { inngest } from "@/lib/inngest/client";
-import { processTask } from "@/lib/inngest/functions";
+import { generateIndustryInsights } from "@/lib/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [
-    processTask,
-  ],
+  functions: [generateIndustryInsights],
 });
